7b947c84c7b11cb8b00a17adbf56e44494eba4a6
Manish Panwar <viz.panwar@gmail.com>
2023-08-05 20:32:51 +0530

    Solved in O(min(s,t))

---
+++ 1. Arrays & Hashing/392. Is Subsequence.py
@@ -0,0 +1,32 @@
+#Problem Statement: Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+#A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of
+#the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence
+#of "abcde" while "aec" is not).
+
+class Solution:
+    def isSubsequence(self, s, t) -> bool:
+    #For s to be subsequence of t, length of s has to be less or equal to t
+        len_s = len(s)
+        len_t = len(t)
+        if len_s > len_t:
+            return False
+    #If character matches increment i.
+        i, j = 0, 0
+        while i != len_s and j != len_t:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+    #If string s was completely exhausted that means s is a subsequence of t. 
+        if i == len_s:
+            return True
+        return False
+
+#Test Cases
+s = "abc"
+t = "ahbgdc"
+print(Solution().isSubsequence(s, t))
+
+s = "axc"
+t = "ahbgdc"
+print(Solution().isSubsequence(s, t))
