30d22d0c73aaa64ecbf1b4d5b29df60817dff266
Manish Panwar <viz.panwar@gmail.com>
2023-08-13 11:16:27 +0530

    Reversed string using two pointers in O(n) and O(1)

---
+++ 2. Two Pointers/344. Reverse String.py
@@ -0,0 +1,21 @@
+#Problem statement: Write a function that reverses a string. The input string is given as an array of characters s.
+
+#You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution:
+    def reverseString(self, s) -> None:
+    #We create two pointers L and R pointing to beginning and ending and swap the values until L < R.
+        L, R = 0, len(s) - 1
+
+        while L < R:
+            s[L], s[R] = s[R], s[L]
+            L, R = L + 1, R - 1
+        
+        return s
+
+#Testcases:
+s = ["h","e","l","l","o"]
+print(Solution().reverseString(s))
+
+s = ["H","a","n","n","a","h"]
+print(Solution().reverseString(s))
