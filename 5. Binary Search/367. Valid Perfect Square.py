50b0c21b9f3701d3193ba3777c31caad3767522f
Manish Panwar <viz.panwar@gmail.com>
2023-08-15 23:07:24 +0530

    Solved in O(logn) TC and O(1) SC

---
+++ 5. Binary Search/367. Valid Perfect Square.py
@@ -0,0 +1,32 @@
+#Problem statement: Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+#A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer
+#with itself.
+
+#You must not use any built-in library function, such as sqrt.
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+    #We initialize L and R to be 1 and num itself. This will be helpful in calculating the middle value and check
+    # if the square of middle value if lesser, greater or equal to num. We changes L, R or return True accordingly.    
+        L, R = 1, num
+
+        while L <= R:
+            mid = (L + R) // 2
+            sqr = mid ** 2
+
+            if sqr > num:
+                R = mid - 1
+            elif sqr < num:
+                L = mid + 1
+            else:
+                return True
+        
+        return False
+
+#Testcases:
+num = 16
+print(Solution().isPerfectSquare(num))
+
+num = 14
+print(Solution().isPerfectSquare(num))
