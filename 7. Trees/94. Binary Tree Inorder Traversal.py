b29118592ec2c9c536b4de94b2576b3cfbd1c103
Manish Panwar <viz.panwar@gmail.com>
2023-08-19 09:49:18 +0530

    Iterative implementation, run in O(n) TC and O(1) SC

--- 7. Trees/94. Binary Tree Inorder Traversal.py
+++ 7. Trees/94. Binary Tree Inorder Traversal.py
@@ -8,6 +8,33 @@ class TreeNode:
         self.right = right
 class Solution:
     def inorderTraversal(self, root):
+    #If root is empty we return None.
+        if root == None:
+            return None
+    
+    #res will store the result of our inorder traversal while stack will be used to store the addresses of root
+    # nodes.
+        res = []
+        stack = []
+        curr = root
+
+    #We keep on traversing left till we encounter None, storing root nodes in stack data.
+        while curr != None:
+            stack.append(curr)
+            curr = curr.left
+
+    #Once we reach None, we pop the last inserted Node in stack, add it to res list and go to root.right
+        while len(stack) > 0:
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+            while curr != None:
+                stack.append(curr)
+                curr = curr.left
+        
+        return res
+
+    def inorderTraversalRecursive(self, root):
     #In order to store result in a list named res, we will need to either declare res outside a function.
     #In order to make the recursive function, we write a recursive function within the function inorderTraversal.
         res = []
