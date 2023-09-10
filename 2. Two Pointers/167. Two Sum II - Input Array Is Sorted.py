87f23a0e01b3efefa10d123211e7c2213ed5ca1a
Manish Panwar <viz.panwar@gmail.com>
2023-09-10 18:22:11 +0530

    Solved in O(n) TC and O(1) SC

---
+++ 2. Two Pointers/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,36 @@
+#Problem Statement: Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+#find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and
+#numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+#Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+#The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+#Your solution must use only constant extra space.
+
+class Solution:
+    def twoSum(self, numbers, target: int):
+        first, last = 0, len(numbers) - 1
+
+        while True:
+            curSum = numbers[first] + numbers[last]
+
+            if curSum > target:
+                last -= 1
+            elif curSum < target:
+                first += 1
+            else:
+                return [first + 1, last + 1]
+
+#Testcases:
+numbers = [2,7,11,15]
+target = 9
+print(Solution().twoSum(numbers, target))
+
+numbers = [2,3,4]
+target = 6
+print(Solution().twoSum(numbers, target))
+
+numbers = [-1,0]
+target = -1
+print(Solution().twoSum(numbers, target))
