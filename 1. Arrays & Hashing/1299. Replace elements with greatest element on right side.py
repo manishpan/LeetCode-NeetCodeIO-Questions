52a4a236b77fc6e192e93d3758f26dfb9a57acb4
Manish Panwar <viz.panwar@gmail.com>
2024-08-31 11:12:51 +0530

    Improved Logic

--- 1. Arrays & Hashing/1299. Replace elements with greatest element on right side.py
+++ 1. Arrays & Hashing/1299. Replace elements with greatest element on right side.py
@@ -5,17 +5,15 @@
 
 class Solution:
     def replaceElements(self, arr):
-    #creating rightmax and currmax. rightmax will hold the maximum element from right of the element, while currmax
-    #will hold the current maximum. Both are initialized to -1
-        rightmax, currmax = -1, -1
+    #creating currmax=-1, we will hold arr[i] to a variable and then replace it with currmax after replacing we will update currmax if tmp > currmax.
+        currmax = -1
 
     #Traversing from right and updating arr[i] as per requirements.
-        for i in range(-1, -len(arr)-1, -1):
-            if arr[i] > currmax:
-                currmax = arr[i]
-            arr[i] = rightmax
-            rightmax = currmax
-    
+        for i in range(len(arr)-1, 0, -1):
+            tmp = arr[i]
+            arr[i] = currmax
+            currmax = max(currmax, tmp)
+
         return arr
 
 arr = [17,18,5,4,6,1]
