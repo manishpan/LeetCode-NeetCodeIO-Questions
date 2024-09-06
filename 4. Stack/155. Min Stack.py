62ce5aa96f7ee7ab696f2cf3a2d9c57700118fa7
Manish Panwar <viz.panwar@gmail.com>
2024-09-06 10:23:10 +0530

    Solved without using any auxillary stack

--- 4. Stack/155. Min Stack.py
+++ 4. Stack/155. Min Stack.py
@@ -9,27 +9,68 @@
 #int getMin() retrieves the minimum element in the stack.
 #You must implement a solution with O(1) time complexity for each function.
 
-class MinStack:
+# Implementation using extra stack
+# class MinStack:
 
+#     def __init__(self):
+#         self.stack = []
+#         self.minStack = []
+        
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+#         if not self.minStack or self.minStack[-1] >= val:
+#             self.minStack.append(val)
+
+#     def pop(self) -> None:
+#         if self.stack[-1] == self.minStack[-1]:
+#             self.minStack.pop()
+#         self.stack.pop()
+        
+#     def top(self) -> int:
+#         return self.stack[-1] 
+
+#     def getMin(self) -> int:
+#         return self.minStack[-1]
+    
+# Implementation without using any secondary stack
+
+class MinStack:
     def __init__(self):
         self.stack = []
-        self.minStack = []
-        
+        self.ME = -1
+
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.minStack or self.minStack[-1] >= val:
-            self.minStack.append(val)
+        if not self.stack:
+            self.stack.append(val)
+            self.ME = val
+        else:
+            if self.ME < val:
+                self.stack.append(val)
+            elif self.ME >= val:
+                self.stack.append(2 * val - self.ME)
+                self.ME = val            
 
     def pop(self) -> None:
-        if self.stack[-1] == self.minStack[-1]:
-            self.minStack.pop()
-        self.stack.pop()
-        
+        if not self.stack:
+            return -1
+        if self.stack[-1] > self.ME:
+            self.stack.pop()
+        elif self.stack[-1] <= self.ME:
+            self.ME = 2 * self.ME - self.stack[-1]
+            self.stack.pop()
+
     def top(self) -> int:
-        return self.stack[-1] 
+        if not self.stack:
+            return -1
+        if self.stack[-1] > self.ME:
+            return self.stack[-1]
+        elif self.stack[-1] <= self.ME:
+            return self.ME
 
     def getMin(self) -> int:
-        return self.minStack[-1]
+        if not self.stack:
+            return -1
+        return self.ME
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
